ad0913b49499023269d5596d48c83b031e70a013
Simeon Naydenov <moniNaydenov@gmail.com>
2015-06-30 14:32:34 +0200

    some more boilerplate

---
+++ locallib.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moni
+ * Date: 15-6-30
+ * Time: 14:15
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class local_simple_message_interface {
+
+}
--- renderer.php
+++ renderer.php
@@ -5,3 +5,10 @@
  * Date: 15-6-30
  * Time: 13:58
  */
+
+
+defined('MOODLE_INTERNAL') || die;
+
+
+class local_simple_message_renderer extends plugin_renderer_base {
+}
