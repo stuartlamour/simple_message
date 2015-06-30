ec89815906d04ef56197ff0b2d0e3c4449b47d07
Stuart Lamour <slamour@moodlerooms.com>
2015-06-30 15:43:46 +0200

    mock data

--- renderer.php
+++ renderer.php
@@ -12,11 +12,36 @@ defined('MOODLE_INTERNAL') || die;
 
 class local_simple_message_renderer extends plugin_renderer_base {
 
+    public function render_navigation() {
+        return "<div id='sm-navigation'>
+        <h2>Direct Messages</h2>
+        <ol>
+          <li>User name <span class='sm-unread'>3</span></li>
+          <li>User name <span class='sm-unread'>3</span></li>
+          <li>User name <span class='sm-unread'>3</span></li>
+        </ol>
+        <h2>My Courses</h2>
+        <ol>
+          <li>Course title <span class='sm-unread'>3</span></li>
+          <li>Course title <span class='sm-unread'>3</span></li>
+          <li>Course title <span class='sm-unread'>3</span></li>
+        </ol>
+        <h2>Group Chat</h2>
+        <ol>
+          <li>Group title <span class='sm-unread'>3</span></li>
+          <li>Group title <span class='sm-unread'>3</span></li>
+          <li>Group title <span class='sm-unread'>3</span></li>
+        </ol>
+        <div>";
+    }
+
     public function render_existing_conversations() {
     }
 
     public function render_conversation() {
+      return "<div id='sm-conversation'>
+              <textarea>your message here</textarea>
+              send
+              </div>"
     }
-
-
 }
