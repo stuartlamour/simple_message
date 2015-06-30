af70e28005be714485e24694238863d0946b30ba
Simeon Naydenov <moniNaydenov@gmail.com>
2015-06-30 14:13:50 +0200

    very very basic boilerplate

---
+++ lang/en/local_simple_message.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moni
+ * Date: 15-6-30
+ * Time: 14:09
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$string['pluginname'] = 'Moodle simple messaging    ';
---
+++ renderer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moni
+ * Date: 15-6-30
+ * Time: 13:58
+ */
